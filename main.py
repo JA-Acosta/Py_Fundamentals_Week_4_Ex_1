ae3263537b9605882de058b220472577355077a3
JAAR <joe.aaron.acosta@outlook.com>
2023-10-09 14:00:07 -0400

    Version 0

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+>>> Program: Generates a program that keeps track of bank account balances and
+allows the user to deposit, withdraw, and display the account balance.
+>>> Author: JAAR
+>>> Date: 10/09/2023
+>>> Version 0
+'''
+
+def main() :
+    pass
+
+if __name__ == '__main__' :
+    main()
